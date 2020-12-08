c34a6addd7e27e494528b8e5ececfae817211cec
Atanas Bozhinov <atanas@cosense.ai>
2020-12-08 15:59:10 +0200

    matrix_operations: Adds sum operation

--- core/matrix_operations.py
+++ core/matrix_operations.py
@@ -9,3 +9,8 @@ def combine(input_x, input_y):
     output = np.append(input_x, input_y, axis=1)
 
     return output
+
+def sum(input_x, input_y):
+    output = np.add(input_x, input_y)
+
+    return output
