6620fb908770c69c6619a8e6fa0b738249e6749e
Atanas Bozhinov <atanas@cosense.ai>
2020-12-08 16:19:18 +0200

    matrix_operations: Adds error handling to pass unit tests

--- core/matrix_operations.py
+++ core/matrix_operations.py
@@ -1,16 +1,25 @@
 import numpy as np
 
 def append(input_x, input_y):
-    output = np.append(input_x, input_y, axis=0)
+    try:
+        output = np.append(input_x, input_y, axis=0)
+    except ValueError:
+        return None
 
     return output
 
 def combine(input_x, input_y):
-    output = np.append(input_x, input_y, axis=1)
+    try:
+        output = np.append(input_x, input_y, axis=1)
+    except ValueError:
+        return None
 
     return output
 
 def sum(input_x, input_y):
+    if not (input_x.shape == input_y.shape):
+        return None
+
     output = np.add(input_x, input_y)
 
     return output
