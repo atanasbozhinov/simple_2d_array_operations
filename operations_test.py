b96cee1edb44fa6da0313b1700fa823c380d9dbd
Atanas Bozhinov <atanas@cosense.ai>
2020-12-08 15:37:27 +0200

    operations_test: Renames core.matrix_operations to mop

--- operations_test.py
+++ operations_test.py
@@ -1,7 +1,7 @@
 
 import unittest
 import numpy as np
-import core.matrix_operations
+import core.matrix_operations as mop
 
 class OperationsTests(unittest.TestCase):
     def test_append(self):
@@ -9,7 +9,7 @@ class OperationsTests(unittest.TestCase):
         input_y = np.array([[7, 8, 9], [10, 11, 12], [13, 14, 15]])
         expected_output = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]])
 
-        test_output = matrix_operations.append(input_x, input_y)
+        test_output = mop.append(input_x, input_y)
 
         self.assertFalse(expected_output == test_output)
 
@@ -18,7 +18,7 @@ class OperationsTests(unittest.TestCase):
         input_y = np.array([[7, 8], [9, 10]])
         expected_output = np.array([[1, 2, 3, 7, 8], [4, 5, 6, 9, 10]])
 
-        test_output = matrix_operations.combine(input_x, input_y)
+        test_output = mop.combine(input_x, input_y)
 
         self.assertFalse(expected_output == test_output)
 
@@ -27,7 +27,7 @@ class OperationsTests(unittest.TestCase):
         input_y = np.array([[7, 8, 9], [10, 11, 12]])
         expected_output = np.array([[8, 10, 12], [14, 16, 18]])
 
-        test_output = matrix_operations.sum(input_x, input_y)
+        test_output = mop.sum(input_x, input_y)
 
         self.assertFalse(expected_output == test_output)
 
